a375cc701d5b17f98077f6004f8d59cfdea0420c
Tony Gibbons <69866149+tonygizmo@users.noreply.github.com>
2026-06-13 11:05:11 +0100

    changes to vehicle checks

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+  $db_host = 'localhost';
+  $db_user = 'u452006220_WeDriveApp';
+  $db_password = 'Coast49tower!';
+  $db_db = 'u452006220_WeDriveApp';
+
+  $mysqli = @new mysqli(
+    $db_host,
+    $db_user,
+    $db_password,
+    $db_db
+  );
+
+  if ($mysqli->connect_error) {
+    echo 'Errno: '.$mysqli->connect_errno;
+    echo '<br>';
+    echo 'Error: '.$mysqli->connect_error;
+    exit();
+  }
+  
+?>
