ac2cf5e3f6f924ffe7768263098dde784aed5ccf
Tony Gibbons <69866149+tonygizmo@users.noreply.github.com>
2026-06-13 11:19:38 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'u452006220_WeDriveApp');
+define('DB_PASSWORD', 'Coast49tower!');
+define('DB_NAME', 'u452006220_WeDriveApp');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
